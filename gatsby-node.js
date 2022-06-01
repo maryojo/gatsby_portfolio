03fc49e9af520d63fb3594521b863b9b1e581398
maryojo <maryojo3@gmail.com>
2022-06-01 09:38:08 +0100

    Debugging build error

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,17 +8,4 @@ exports.createPages = async ({ actions }) => {
   })
 }
 
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /bad-module/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    })
-  }
-}
+
